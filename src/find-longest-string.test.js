2ec782f322c496ba410970ac0ab8509add6c48a6
Sheldon01 <zhtm415@live.rhul.ac.uk>
2024-06-06 15:48:10 +0100

    added shortest-string file and shortest-string-test file

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,7 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["america", "mexico"])).toBe("america");
+  expect(findLongestString(["pitbull", "toenail"])).toBe("pitbull");
+
 });
